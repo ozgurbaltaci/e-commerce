87a9a95358e46f0bdb5bbae858baed9bc3cd44aa
ozgurbaltaci <wozgurbaltaci@gmail.com>
2023-08-15 01:05:50 +0300

    Global font family added, refs #3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import logo from "./logo.svg";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 import MainPage from "./MainPage";
 import Cart from "./Cart";
 import Favorites from "./Favorites";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Cabin, sans-serif",
+    fontSize: 12,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+
+    h1: {
+      fontSize: 36,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h2: {
+      fontSize: 28,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h3: {
+      fontSize: 22,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h4: {
+      fontSize: 18,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h5: {
+      fontSize: 16,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h6: {
+      fontSize: 14,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+  },
+});
 
 function App() {
   return (
-    <Routes>
-      <Route path="*" element={<Navigate to="/" />} />
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
 
-      <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
 
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/favorites" element={<Favorites />} />
-    </Routes>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
