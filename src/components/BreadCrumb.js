a1e119734bf0996730427c0bfd35fa4f373f800c
ozgurbaltaci <wozgurbaltaci@gmail.com>
2024-02-05 15:14:29 +0300

    Update BreadCrumb, Add css file, #57

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,10 +1,9 @@
 import React from "react";
-import classes from "./Breadcrumb.module.css";
+import classes from "./BreadCrumb.module.css";
 import { IconContext } from "react-icons";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Breadcrumb = (props) => {
-  const n = 4;
   return (
     <div>
       <div className={classes.breadCrumb}>
@@ -13,7 +12,9 @@ const Breadcrumb = (props) => {
             <div>
               <a href={e.navigation} className={classes.breadcrumb_item}>
                 <span>{e.name}</span>
-                {i !== n - 1 && <MdOutlineKeyboardArrowRight color="#2FB009" />}
+                {i !== props.breadCrumbData.length - 1 && (
+                  <MdOutlineKeyboardArrowRight color="#2FB009" />
+                )}
               </a>
             </div>
           );
