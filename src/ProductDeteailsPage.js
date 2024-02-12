e984f3f1bb9fbfbdc5acdaa7eb66bfb71bb5fb95
ozgurbaltaci <wozgurbaltaci@gmail.com>
2024-02-12 12:09:59 +0300

    Update ProductDeteails, Add comment input, #28,#29

--- src/ProductDeteailsPage.js
+++ src/ProductDeteailsPage.js
@@ -2,12 +2,35 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import { Divider } from "@mui/material";
 import MyButton from "./components/MyButton";
+import { TiStarOutline } from "react-icons/ti";
+import { TiStarFullOutline, TiStarHalfOutline } from "react-icons/ti";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { FavoriteBorder } from "@material-ui/icons";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& $notchedOutline": {
+            borderColor: "green", // Change this to your desired border color
+          },
+        },
+      },
+    },
+  },
+});
 
 const ProductDeteailsPage = () => {
   const [productDetails, setProductDetails] = useState([]);
   const { productId } = useParams();
+  const [commentInput, setCommentInput] = useState("");
+  const [comments, setComments] = useState([]);
+  const [isReviewInputFocused, setIsReviewInputFocused] = useState(false);
   useEffect(() => {
     try {
       axios
@@ -17,15 +40,78 @@ const ProductDeteailsPage = () => {
         });
     } catch (err) {}
   }, []);
+
+  const renderStars = (starPoint) => {
+    const starElements = [];
+    const fullStars = Math.floor(starPoint);
+    const hasHalfStar = starPoint - fullStars >= 0.5;
+
+    for (let i = 0; i < 5; i++) {
+      if (i < fullStars) {
+        starElements.push(
+          <TiStarFullOutline
+            key={i}
+            style={{ color: "rgb(245, 195, 69)", fontSize: "18px" }}
+          />
+        );
+      } else if (i === fullStars && hasHalfStar) {
+        starElements.push(
+          <TiStarHalfOutline
+            key={i}
+            style={{ color: "rgb(245, 195, 69)", fontSize: "18px" }}
+          />
+        );
+      } else {
+        starElements.push(
+          <TiStarOutline key={i} style={{ fontSize: "18px" }} />
+        );
+      }
+    }
+
+    return starElements;
+  };
+
+  const handleCommentInputChange = (event) => {
+    setCommentInput(event.target.value);
+  };
+
+  const handleSubmitComment = async () => {
+    try {
+      if (commentInput.trim() !== "") {
+        const response = await axios.post(
+          `http://localhost:3002/saveRatingAndReview/${localStorage.getItem(
+            "user_id"
+          )}/${productId}`,
+          {
+            ratingPoint: 1,
+            review: commentInput,
+          }
+        );
+        setComments([...comments, commentInput]);
+        setCommentInput(""); // Clear input field after submission
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleReviewInputFocus = () => {
+    setIsReviewInputFocused(true);
+  };
+
+  const handleReviewInputBlur = () => {
+    setIsReviewInputFocused(false);
+  };
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div style={{ display: "flex" }}>
         <div className="imageDetails">
           <img
             src={productDetails.image}
             style={{
-              width: "550px",
-              height: "532px",
+              width: "500px",
+              height: "484px",
               border: "1px solid #EEEEEE",
               borderRadius: "7px",
             }}
@@ -37,20 +123,71 @@ const ProductDeteailsPage = () => {
           style={{
             marginLeft: "30px",
             width: "100%",
-            height: "532px",
+            height: "484px",
             position: "relative",
           }}
         >
-          <div style={{ fontSize: "28px", fontWeight: "bold" }}>
+          <div style={{ fontSize: "26px", fontWeight: "bold" }}>
             {productDetails.manufacturer_name}
           </div>
-          <div style={{ fontSize: "24px" }}>{productDetails.description}</div>
-          <div className="priceInfo" style={{ padding: "10px 0px" }}>
+          <div
+            style={{
+              fontSize: "21px",
+              maxHeight: "54px",
+              overflow: "hidden",
+              lineHeight: "27px",
+            }}
+          >
+            {productDetails.description}
+          </div>
+
+          <div className="ratingStars">
+            <div
+              style={{
+                display: "flex",
+                fontSize: "12px",
+                margin: "5px 0px",
+                alignItems: "center",
+                width: "fit-content",
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div style={{ fontWeight: "bold", marginRight: "5px" }}>
+                {`${
+                  productDetails.starPoint
+                    ? productDetails.starPoint.toFixed(1)
+                    : 0.0
+                }`}
+              </div>
+
+              {renderStars(productDetails.starPoint)}
+              <FiberManualRecordIcon
+                style={{
+                  color: "#999999",
+                  fontSize: "6px",
+                  margin: "0px 2px",
+                }}
+              ></FiberManualRecordIcon>
+              <span
+                style={{
+                  marginLeft: "2px",
+                  fontSize: "10px",
+                  fontStyle: "italic",
+                  cursor: "pointer",
+                }}
+              >{`${
+                productDetails.ratings_count && productDetails.ratings_count
+              } Review(s)`}</span>
+            </div>
+          </div>
+          <div className="priceInfo">
             <div
               style={{
                 fontWeight: "bold",
                 fontSize:
-                  productDetails.discounted_price === null ? "32px" : "22px",
+                  productDetails.discounted_price === null ? "25px" : "14px",
                 textDecoration:
                   productDetails.discounted_price === null
                     ? "none"
@@ -67,7 +204,7 @@ const ProductDeteailsPage = () => {
               <div
                 style={{
                   color: "#00990F",
-                  fontSize: "32px",
+                  fontSize: "25px",
                   fontWeight: "bold",
                 }}
               >
@@ -77,22 +214,57 @@ const ProductDeteailsPage = () => {
           </div>
           {console.log(productDetails)}
 
-          <Divider></Divider>
-          <div className="categoryName">
-            Category Name: {productDetails.category_name}
-          </div>
-          <div className="categoryName">
-            Sub Category Name: {productDetails.sub_category_name}
-          </div>
-          <div className="categoryName">
-            Stock Quantity: {productDetails.stock_quantity}
+          <Divider style={{ margin: "5px 0px" }}></Divider>
+          <div style={{ fontSize: "12px" }}>
+            <div
+              className="categoryName"
+              style={{ marginBottom: "5px", display: "flex" }}
+            >
+              <span style={{ width: "135px", fontWeight: "bold" }}>
+                Category Name
+              </span>
+              <div style={{ fontWeight: "bold" }}>:</div>
+
+              <div style={{ marginLeft: "7px" }}>
+                {productDetails.category_name}
+              </div>
+            </div>
+            <div
+              className="subCategoryName"
+              style={{ marginBottom: "5px", display: "flex" }}
+            >
+              <span style={{ width: "135px", fontWeight: "bold" }}>
+                Sub-Category Name
+              </span>
+              <div style={{ fontWeight: "bold" }}>:</div>
+
+              <div style={{ marginLeft: "7px" }}>
+                {productDetails.sub_category_name}
+              </div>
+            </div>
+            <div
+              className="stockQuantity"
+              style={{ marginBottom: "5px", display: "flex" }}
+            >
+              <span style={{ width: "135px", fontWeight: "bold" }}>
+                Stock Quantity
+              </span>
+              <div style={{ fontWeight: "bold" }}>:</div>
+              <div style={{ marginLeft: "7px" }}>
+                {productDetails.stock_quantity}
+              </div>
+            </div>
           </div>
 
-          <div className="actionButtons">
+          <div
+            className="actionButtons"
+            style={{ display: "flex", margin: "10px 0px" }}
+          >
             <button
               style={{
                 width: "100%",
                 height: "40px",
+                marginRight: "10px",
 
                 backgroundColor: "#2FB009",
                 borderRadius: "3px",
@@ -112,6 +284,20 @@ const ProductDeteailsPage = () => {
             >
               {"Add to Cart"}
             </button>
+
+            <div
+              style={{
+                width: "40px", // Specific width for the circle
+                height: "40px", // Same height as width to ensure a perfect circle
+                borderRadius: "50%", // Makes it a circle
+                border: "1px solid #9999",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <FavoriteBorder style={{ fontSize: 22 }} />
+            </div>
           </div>
           <div
             className="infoBox"
@@ -172,7 +358,59 @@ const ProductDeteailsPage = () => {
           </div>
         </div>
       </div>
-    </>
+
+      <div
+        className="reviews_and_ratings"
+        style={{
+          padding: "20px 0px 20px 28px",
+          border: "1px solid #9999 ",
+          borderRadius: "7px",
+        }}
+      >
+        <div>
+          <h3>Rate the Product: </h3>
+          <textarea
+            value={commentInput}
+            onChange={handleCommentInputChange}
+            placeholder="Type your comment here..."
+            style={{
+              minHeight: "100px",
+              display: "block",
+              width: "calc(100% - 28px)",
+            }}
+          />
+          <button
+            style={{
+              width: "180px",
+              padding: "8px",
+              marginTop: "8px",
+              backgroundColor: "#2FB009",
+              borderRadius: "3px",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "none",
+              color: "white",
+              fontWeight: "bold",
+              fontSize: "13px",
+              fontFamily: "Cabin",
+            }}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmitComment}
+          >
+            {"Save your rating"}
+          </button>
+        </div>
+        <div>
+          <h2>Comments</h2>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 };
 
